3bccc1b7b16e81130484bed864bf56770c94a815
Joenell Leina'ala Dietmeyer <83037402+leiDiet@users.noreply.github.com>
2026-03-08 19:57:37 -0500

    Update train.py

--- data-science/src/train.py
+++ data-science/src/train.py
@@ -67,7 +67,7 @@ def main(args):
     mlflow.log_metric("MSE", float(mse))
     
     # Save the model
-    mlflow.sklearn.log_model(sk_model=model, artifact_path=args.model_output)
+    mlflow.sklearn.save_model(sk_model=model, path=args.model_output)
 
 if __name__ == "__main__":
     mlflow.start_run()
